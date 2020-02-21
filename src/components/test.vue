1f11039a14af29e12ab1007ecf91ebe01d615791
hushuxian <Fischfilet@126.com>
2020-02-21 16:16:06 +0800

    test

---
+++ src/components/test.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+<script type="text/javascript">
+  export default {
+    name: 'component_name',
+    data () {
+      return {
+      };
+    },
+    methods: {
+
+    },
+  };
+</script>
+<style rel="stylesheet/stylus" lang="stylus" scoped>
+
+</style>
